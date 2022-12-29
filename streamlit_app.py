8ec635b4de6ccedfcca55ee9aa786f62840c692f
TMCC <121591981+tmccsnowflake@users.noreply.github.com>
2022-12-28 19:12:42 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 streamlit.title('🥣 My Parents New Healthy Diner')
 streamlit.header('🥗 Breakfast Favorites')
 streamlit.text('🐔 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥑Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🍞 Hard-Boiled Free-Range Egg')
 streamlit.text('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
