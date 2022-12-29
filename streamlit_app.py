1018ff788215ee8d25eb6c8b9d3d7072d74f7e61
TMCC <121591981+tmccsnowflake@users.noreply.github.com>
2022-12-28 19:12:22 -0800

    Update streamlit_app.py
    Figured out how to add emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
-streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Favorites')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-streamlit.text('Build Your Own Fruit Smoothie')
+streamlit.title('🥣 My Parents New Healthy Diner')
+streamlit.header('🥗 Breakfast Favorites')
+streamlit.text('🐔 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥑Kale, Spinach & Rocket Smoothie')
+streamlit.text('🍞 Hard-Boiled Free-Range Egg')
+streamlit.text('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
