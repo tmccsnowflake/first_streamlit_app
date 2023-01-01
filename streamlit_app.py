72d9085bd0d346907a3bc91a607a6ff12e397760
TMCC <121591981+tmccsnowflake@users.noreply.github.com>
2023-01-01 14:58:03 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
 import streamlit
 streamlit.title('🥣 My Parents New Healthy Diner')
 streamlit.header('🥗 Breakfast Favorites')
