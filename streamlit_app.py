6d4e1ceb2108dd0deab1241d85109b114006d454
TMCC <121591981+tmccsnowflake@users.noreply.github.com>
2023-01-01 15:01:24 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,3 @@
-
-
 import streamlit
 streamlit.title('🥣 My Parents New Healthy Diner')
 streamlit.header('🥗 Breakfast Favorites')
@@ -18,3 +16,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
