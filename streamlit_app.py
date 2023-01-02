5df64d0bccc91d0a1a7cdefe15308a536bd312b1
TMCC <121591981+tmccsnowflake@users.noreply.github.com>
2023-01-01 17:48:17 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,22 +46,20 @@ except URLError as e:
   
 streamlit.write('The user entered ', fruit_choice)
 
-#import requests
-#take the json version of the response and normalize it
-#output it the screen as a table
-#import snowflake.connector
-
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
+
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
+#Line 61 in the class, below
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
           my_cur.execute("insert into fruit_load_list values('" + new_fruit +"')")
           return "Thanks for adding " + new_fruit
-        
+
+#Line 66 in the class,below
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a fruit to the list'):
     my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
