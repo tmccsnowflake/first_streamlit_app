ebeaf920d3284e065f751aac45609ef05288099a
TMCC <121591981+tmccsnowflake@users.noreply.github.com>
2023-01-01 17:31:22 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,7 @@ streamlit.dataframe(my_data_rows)
 
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-          my_cur.execute("insert into fruit_load_list values('" + fruit_choice +"')")
+          my_cur.execute("insert into fruit_load_list values('" + new_fruit +"')")
           return "Thanks for adding " + new_fruit
         
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
