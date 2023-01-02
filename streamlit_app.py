94cf2725ee6abfe5302fe705d80b9d554d731210
TMCC <121591981+tmccsnowflake@users.noreply.github.com>
2023-01-01 16:57:50 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,4 +40,6 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('Now what fruit would you like information about?','jackfruit')
-streamlit.write('The user entered ', add_my_fruit)
+streamlit.write('Thanks for adding ', add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list values('from streamlit')");
